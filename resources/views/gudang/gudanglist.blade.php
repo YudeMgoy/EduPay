cfb417320012603228179b76e89236dd6d05ced2
YudeMgoy <yudemgoy25@gmail.com>
2019-10-17 06:42:25 +0700

    style and add migration

--- resources/views/gudang/gudanglist.blade.php
+++ resources/views/gudang/gudanglist.blade.php
@@ -5,32 +5,70 @@
             Daftar Pesanan
     </h4>
     <div class="list-pesanan">
-        @foreach ($transaksis as $transaksi)
+        @php
+            $index =0;
+        @endphp
+        @foreach ($transaksis as $transaksi)        
             <div class="pesanan-box">
-                <div class="pesanan-header">
-                    <h4>Pembeli: {{$transaksi->get_barang->name}}</h4>
+                <div class="pesanan-header" onclick="showBody(<?php echo $index ?>)">
+                    <h4>{{$transaksi->get_barang->name}}</h4>
+                    <i class="fa fa-caret-down"></i>
                 </div>
-                <div class="pesanan-body"> 
-                    <p class="orange">List barang yang di beli <i class="fas fa-chevron-down"></i></p>
-                    <div class="barang-list">
-                        @foreach ($transaksi->get_keranjang as $keranjang)
+                <div class="pesanan-body">                     
+                    <div class="barang-pesanan-list">
+                        {{-- @foreach ($transaksi->get_keranjang as $keranjang)
                             <div class="barang-box">
                                 <h4>{{ $keranjang->get_barang->nama_barang }}</h4>                                
                                 <p>{{ $keranjang->get_barang->harga_barang }}/kg ({{ $keranjang->jumlah_barang }})</p>
                             </div>                                                        
-                        @endforeach
+                        @endforeach --}}
+                        <div class="barang-pesanan-box">
+                            <h4>Air Mineral</h4>
+                            <div class="bottom">
+                                <p>Rp 1.500,00/pcs (3)</p>
+                                <a href="#" class="orange">Kosong</a>
+                            </div>                                                            
+                        </div>
+                        <div class="barang-pesanan-box">
+                            <h4>Air Mineral</h4>                                
+                            <div class="bottom">
+                                <p>Rp 1.500,00/pcs (3)</p>
+                                <a href="#" class="orange">Kosong</a>
+                            </div>
+                        </div>
+                        <div class="barang-pesanan-box">
+                            <h4>Air Mineral</h4>                                
+                            <div class="bottom">
+                                <p>Rp 1.500,00/pcs (3)</p>
+                                <a href="#" class="orange">Kosong</a>
+                            </div>
+                        </div>
                     </div>                                    
                 </div>
 
                 <div class="pesanan-footer">
-                    <h6>Metode Bayar : {{ $transaksi->get_pay->pay }}</h6>                                        
-                    <h6>Status : {{ $transaksi->get_status->status }}</h6>                                                                                
-                    <h6>
-                        Total Bayar :   {{$transaksi->total_harga}}
-                    </h6>
+                    <div class="status">
+                        <p>Metode : {{ $transaksi->get_pay->pay }}</p>
+                        <p class="{{$transaksi->get_status->style}}">{{ $transaksi->get_status->status }}</p>
+                    </div>                    
+                    <div class="bayar">
+                        <h6>Total Bayar :  </h6>
+                        <h6>Rp {{number_format($transaksi->total_harga, 2, ',','.')}} </h6>
+                    </div>                    
                 </div>
             </div>
+            @php
+                $index++;
+            @endphp
         @endforeach
     </div>
 </div>
+
+<script>
+    bodies = document.getElementsByClassName("pesanan-body");
+
+    function showBody(index){        
+        bodies[index].classList.toggle('pesanan-body-active');
+    }
+</script>
 @endsection
